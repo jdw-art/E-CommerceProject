6d8202b425a6a5be8529914536a97a2630245e9b
jdw-art <jdw17733356203@icloud.com>
2024-02-29 21:43:04 +0800

    Update UmsMemberReceiveAddressServiceImpl

---
+++ mall-portal/src/main/java/com/macro/mall/portal/service/UmsMemberCouponService.java
@@ -0,0 +1,10 @@
+package com.macro.mall.portal.service;
+
+/**
+ * @Author: Jacob
+ * @Description: TODO
+ * @Date: 2024/2/29 21:41
+ * @Version: 1.0
+ */
+public interface UmsMemberCouponService {
+}
