e330401d4352bab74e86378486bd5c316a4d86bd
jdw-art <jdw17733356203@icloud.com>
2024-02-29 20:30:50 +0800

    Update OrderParam.java

--- mall-portal/src/main/java/com/macro/mall/portal/domain/OrderParam.java
+++ mall-portal/src/main/java/com/macro/mall/portal/domain/OrderParam.java
@@ -1,10 +1,28 @@
 package com.macro.mall.portal.domain;
 
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
 /**
  * @Author: Jacob
  * @Description: TODO 生成订单时传入的参数
  * @Date: 2024/2/29 16:24
  * @Version: 1.0
  */
+@Getter
+@Setter
 public class OrderParam {
+    @ApiModelProperty("收货地址ID")
+    private Long memberReceiverAddressId;
+    @ApiModelProperty("优惠券ID")
+    private Long couponId;
+    @ApiModelProperty("使用的积分数")
+    private Integer useIntegration;
+    @ApiModelProperty("支付方式")
+    private Integer payType;
+    @ApiModelProperty("被选中的购物车商品ID")
+    private List<Long> cartIds;
 }
