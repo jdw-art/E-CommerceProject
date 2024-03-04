42905468702d6e3047e292069037b4bca2c5480d
jdw-art <jdw17733356203@icloud.com>
2024-03-04 19:42:45 +0800

    Update MemberAttentionService.java Member brand attention package

---
+++ mall-portal/src/main/java/com/macro/mall/portal/domain/MemberBrandAttention.java
@@ -0,0 +1,33 @@
+package com.macro.mall.portal.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+
+/**
+ * @Author: Jacob
+ * @Description: 会员品牌关注
+ * @Date: 2024/3/4 19:37
+ * @Version: 1.0
+ */
+@Getter
+@Setter
+@Document
+public class MemberBrandAttention {
+    @Id
+    private String id;
+    @Indexed
+    private Long memberId;
+    private String memberNickname;
+    private String memberIcon;
+    @Indexed
+    private Long brandId;
+    private String brandName;
+    private String brandLogo;
+    private String brandCity;
+    private Date crateTime;
+}
