cb27427cb7584540a4631a6103aabd9cff425c1c
jdw-art <jdw17733356203@icloud.com>
2024-03-04 21:09:17 +0800

    Update MemberBrandAttentionRepository.java Member Brands Follow Repository

---
+++ mall-portal/src/main/java/com/macro/mall/portal/repository/MemberBrandAttentionRepository.java
@@ -0,0 +1,34 @@
+package com.macro.mall.portal.repository;
+
+import com.macro.mall.portal.domain.MemberBrandAttention;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+/**
+ * @Author: Jacob
+ * @Description: 会员品牌关注Repository
+ * @Date: 2024/3/4 19:50
+ * @Version: 1.0
+ */
+public interface MemberBrandAttentionRepository extends MongoRepository<MemberBrandAttention, String> {
+    /**
+     * 根据会员ID和品牌ID查找记录
+     */
+    MemberBrandAttention findByMemberIdAndBrandId(Long memberId, Long brandId);
+
+    /**
+     * 根据会员ID和品牌ID删除记录
+     */
+    int deleteByMemberIdAndBrandId(Long memberId, Long brandId);
+
+    /**
+     * 根据会员ID分页查找记录
+     */
+    Page<MemberBrandAttention> findByMemberId(Long memberId, Pageable pageable);
+
+    /**
+     * 根据会员ID删除记录
+     */
+    int deleteAllByMemberId(Long memberId);
+}
