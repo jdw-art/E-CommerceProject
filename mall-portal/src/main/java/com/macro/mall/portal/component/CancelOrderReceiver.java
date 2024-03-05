6617f07c6c311f1bcf18e924d451c3a24e786aff
jdw-art <jdw17733356203@icloud.com>
2024-03-05 19:00:27 +0800

    fix(rabbitMQ):missing @RabbitHandler annotation in handle method

--- mall-portal/src/main/java/com/macro/mall/portal/component/CancelOrderReceiver.java
+++ mall-portal/src/main/java/com/macro/mall/portal/component/CancelOrderReceiver.java
@@ -3,6 +3,7 @@ package com.macro.mall.portal.component;
 import com.macro.mall.portal.service.OmsPortalOrderService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -20,6 +21,7 @@ public class CancelOrderReceiver {
     @Autowired
     private OmsPortalOrderService portalOrderService;
 
+    @RabbitHandler
     public void handle(Long orderId) {
         portalOrderService.cancelOrder(orderId);
         LOGGER.info("process orderId:{}", orderId);
