20e07673641f869362a62427a2bfe47d548734b9
jdw-art <jdw17733356203@icloud.com>
2024-03-05 10:03:50 +0800

    Update MemberProductCollection.java Member product collection domain

---
+++ mall-portal/src/main/java/com/macro/mall/portal/domain/MemberProductCollection.java
@@ -0,0 +1,34 @@
+package com.macro.mall.portal.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+
+/**
+ * @Author: Jacob
+ * @Description: 会员商品收藏
+ * @Date: 2024/3/4 21:12
+ * @Version: 1.0
+ */
+@Getter
+@Setter
+@Document
+public class MemberProductCollection {
+    @Id
+    private String id;
+    @Indexed
+    private Long memberId;
+    private String memberNickname;
+    private String memberIcon;
+    @Indexed
+    private Long productId;
+    private String productName;
+    private String productPic;
+    private String productSubTitle;
+    private String productPrice;
+    private Date createTime;
+}
